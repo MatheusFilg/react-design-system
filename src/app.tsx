e0f90f54ff2c2029b7f3fc8047064fc8d8d032df
matheusf <matheus_falmeida@hotmail.com>
2024-08-13 21:42:12 -0300

    add label variants

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,9 @@ import Label from './components/label'
 export default function App() {
   return (
     <div className="flex flex-col gap-6 p-8">
-      <div className="flex flex-row items-center align-middle">
+      <div className="flex flex-row items-center gap-6 align-middle">
         <Label>Primary</Label>
-        <div className="flex translate-x-6 flex-row gap-10">
+        <div className="flex flex-row gap-10">
           <Button variant="primary">Button</Button>
           <Button disabled>Button</Button>
         </div>
@@ -55,12 +55,27 @@ export default function App() {
       <div className="flex flex-row items-center align-middle">
         <Label>Ghost</Label>
         <div className="flex translate-x-6 flex-row gap-10">
+          {/* <div className="flex flex-col items-center gap-5">
+          </div> */}
           <Button variant="ghost" size="small">
             Button
           </Button>
+          {/* <Label variant="vertical">Vertical</Label> */}
           <Button disabled size="small">
             Button
           </Button>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center align-middle">
+        <div className="h-8 w-24" />
+        <div className="flex translate-x-6 flex-row gap-8">
+          <div className="flex items-center">
+            <Label variant="vertical">Default</Label>
+          </div>
+          <div>
+            <Label variant="vertical">Disabled</Label>
+          </div>
         </div>
       </div>
     </div>
--- src/components/label.tsx
+++ src/components/label.tsx
@@ -2,11 +2,67 @@ import { ReactNode } from 'react'
 
 interface LabelProps {
   children: ReactNode
+  variant?: 'horizontal' | 'vertical' | 'group'
+  defaultStyle?: string
 }
 
-export default function Label({ children }: LabelProps) {
+export default function Label(props: LabelProps) {
+  const { variant } = props
+
+  const style = 'w-fit items-center justify-center border border-[#B2B2B2]'
+
+  switch (variant) {
+    case 'horizontal':
+      return <HorizontalLabel defaultStyle={style} {...props} />
+
+    case 'vertical':
+      return <VerticalLabel defaultStyle={style} {...props} />
+
+    case 'group':
+      return <GroupLabel defaultStyle={style} {...props} />
+
+    default:
+      return <HorizontalLabel defaultStyle={style} {...props} />
+  }
+}
+
+export function HorizontalLabel(props: LabelProps) {
+  const { children, defaultStyle } = props
+
   return (
-    <div className="flex flex-row items-center justify-center">
+    <div className="flex w-[100px] flex-row items-center">
+      <label
+        {...props}
+        className={`${defaultStyle} flex h-8 rounded-lg bg-[#CDCDCD] px-2 py-1`}
+      >
+        {children}
+      </label>
+      <div className="h-[1px] w-6 bg-[#E3E3E3]" />
+    </div>
+  )
+}
+
+export function VerticalLabel(props: LabelProps) {
+  const { children, defaultStyle } = props
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="h-6 w-[2px] bg-[#E3E3E3]" />
+      <label
+        {...props}
+        className={`${defaultStyle} flex h-8 rounded-lg bg-[#E3E3E3] px-2 py-1`}
+      >
+        {children}
+      </label>
+    </div>
+  )
+}
+
+export function GroupLabel(props: LabelProps) {
+  const { children } = props
+
+  return (
+    <div className="flex flex-row items-center">
       <label className="flex h-8 w-20 items-center justify-center rounded-lg border border-[#B2B2B2] bg-[#CDCDCD] px-4 py-2">
         {children}
       </label>
