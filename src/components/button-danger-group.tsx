7258c20e477d39cd9a12917f5884df66e153f972
matheusf <matheus_falmeida@hotmail.com>
2024-09-07 21:29:56 -0300

    fix labels position

--- src/components/button-danger-group.tsx
+++ src/components/button-danger-group.tsx
@@ -5,7 +5,7 @@ export default function ButtonDangerGroup() {
   return (
     <div className="flex flex-col gap-10">
       {/* Medium Buttons */}
-      <div className="flex flex-row items-center gap-10">
+      <div className="flex flex-row gap-10">
         <Label variant="group">Medium</Label>
 
         <div className="flex flex-col gap-10">
@@ -28,13 +28,13 @@ export default function ButtonDangerGroup() {
       </div>
 
       {/* Small Buttons */}
-      <div className="flex flex-row items-center gap-10">
+      <div className="flex translate-x-2 flex-row items-center gap-10">
         <Label variant="group">Small</Label>
 
         <div className="flex flex-col gap-10">
           <div className="flex flex-row items-center gap-6 align-middle">
             <Label>Primary</Label>
-            <div className="flex flex-row gap-10">
+            <div className="flex flex-row gap-14">
               <ButtonDanger size="small" variant="primary">
                 Button
               </ButtonDanger>
@@ -46,7 +46,7 @@ export default function ButtonDangerGroup() {
 
           <div className="flex flex-row items-center align-middle">
             <Label>Ghost</Label>
-            <div className="flex translate-x-6 flex-row gap-10">
+            <div className="flex translate-x-6 flex-row gap-14">
               <ButtonDanger size="small" variant="ghost">
                 Button
               </ButtonDanger>
@@ -60,7 +60,7 @@ export default function ButtonDangerGroup() {
 
       <div className="flex flex-row items-center align-middle">
         <div className="w-52" />
-        <div className="flex -translate-y-10 translate-x-12 flex-row gap-4">
+        <div className="flex -translate-y-10 translate-x-16 flex-row gap-6">
           <div className="flex items-center">
             <Label variant="vertical">Default</Label>
           </div>
--- src/components/button-group.tsx
+++ src/components/button-group.tsx
@@ -36,13 +36,13 @@ export default function ButtonGroup() {
       </div>
 
       {/* Small Buttons */}
-      <div className="flex flex-row gap-10">
+      <div className="flex translate-x-2 flex-row gap-10">
         <Label variant="group">Small</Label>
 
         <div className="flex flex-col gap-10">
           <div className="flex flex-row items-center align-middle">
             <Label>Primary</Label>
-            <div className="flex translate-x-6 flex-row gap-10">
+            <div className="flex translate-x-6 flex-row gap-14">
               <Button variant="primary" size="small">
                 Button
               </Button>
@@ -54,7 +54,7 @@ export default function ButtonGroup() {
 
           <div className="flex flex-row items-center align-middle">
             <Label>Neutral</Label>
-            <div className="flex translate-x-6 flex-row gap-10">
+            <div className="flex translate-x-6 flex-row gap-14">
               <Button variant="neutral" size="small">
                 Button
               </Button>
@@ -66,7 +66,7 @@ export default function ButtonGroup() {
 
           <div className="flex flex-row items-center align-middle">
             <Label>Ghost</Label>
-            <div className="flex translate-x-6 flex-row gap-10">
+            <div className="flex translate-x-6 flex-row gap-14">
               <Button variant="ghost" size="small">
                 Button
               </Button>
@@ -80,7 +80,7 @@ export default function ButtonGroup() {
 
       <div className="flex flex-row items-center align-middle">
         <div className="w-52" />
-        <div className="flex -translate-y-10 translate-x-12 flex-row gap-4">
+        <div className="flex -translate-y-10 translate-x-14 flex-row gap-10">
           <div className="flex items-center">
             <Label variant="vertical">Default</Label>
           </div>
