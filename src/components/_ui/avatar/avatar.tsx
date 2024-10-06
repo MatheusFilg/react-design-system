0f382cdbd48e848e3bd1d3927218271f5142bc59
matheusf <matheus_falmeida@hotmail.com>
2024-10-05 22:25:41 -0300

    finish avatar component

--- src/components/_ui/avatar/avatar.tsx
+++ src/components/_ui/avatar/avatar.tsx
@@ -1,18 +1,41 @@
-interface AvatarProps {
+import { AnchorHTMLAttributes } from 'react'
+
+interface AvatarProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   size?: 'small' | 'medium' | 'large'
   format?: 'square' | 'circle'
   src?: string
 }
 
 export default function Avatar(props: AvatarProps) {
-  const { src } = props
+  const { src, size = 'large', format = 'square' } = props
+
+  let variantStyle = ''
+
+  if (size === 'large') {
+    variantStyle += 'h-10 w-10 text-base '
+  }
+  if (size === 'medium') {
+    variantStyle += 'h-8 w-8 text-sm '
+  }
+  if (size === 'small') {
+    variantStyle += 'h-6 w-6 text-sm '
+  }
+
+  if (format === 'square') {
+    variantStyle += 'rounded-lg '
+  }
+  if (format === 'circle') {
+    variantStyle += 'rounded-full '
+  }
 
   return (
-    <div className="">
+    <div>
       {src ? (
-        <img src={src} alt="" className="h-10 w-10 rounded" />
+        <img src={src} alt="" className={`${variantStyle} `} />
       ) : (
-        <p className="flex h-10 w-10 items-center justify-center rounded bg-black text-white">
+        <p
+          className={`${variantStyle} flex items-center justify-center bg-black font-inter text-white`}
+        >
           M
         </p>
       )}
--- src/components/avatar-group/avatar-group.tsx
+++ src/components/avatar-group/avatar-group.tsx
@@ -4,20 +4,105 @@ import Label from '../_ui/label'
 export default function AvatarGroup() {
   return (
     <div className="flex flex-col gap-10">
+      {/* Large Avatar */}
+      <div className="flex translate-x-2 flex-row items-center gap-10">
+        <Label variant="group">Large</Label>
+
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-row items-center gap-6 align-middle">
+            <Label>Circle</Label>
+            <div className="flex gap-14">
+              <Avatar
+                src="https://github.com/MatheusFilg.png"
+                size="large"
+                format="circle"
+              />
+              <Avatar size="large" format="circle" />
+            </div>
+          </div>
+
+          <div className="flex flex-row items-center align-middle">
+            <Label>Square</Label>
+            <div className="flex translate-x-6 flex-row gap-14">
+              <Avatar
+                src="https://github.com/MatheusFilg.png"
+                size="large"
+                format="square"
+              />
+              <Avatar size="large" format="square" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Medium Avatar */}
       <div className="flex flex-row items-center gap-10">
         <Label variant="group">Medium</Label>
 
         <div className="flex flex-col gap-10">
           <div className="flex flex-row items-center gap-6 align-middle">
-            <Label>Row</Label>
-            <div className="flex gap-10">
-              <Avatar src="https://github.com/MatheusFilg.png" />
+            <Label>Circle</Label>
+            <div className="flex gap-14">
+              <Avatar
+                src="https://github.com/MatheusFilg.png"
+                size="medium"
+                format="circle"
+              />
+              <Avatar size="medium" format="circle" />
             </div>
           </div>
 
           <div className="flex flex-row items-center align-middle">
-            <Label>Column</Label>
-            <div className="flex translate-x-10 flex-row gap-10"></div>
+            <Label>Square</Label>
+            <div className="flex translate-x-6 flex-row gap-14">
+              <Avatar
+                src="https://github.com/MatheusFilg.png"
+                size="medium"
+                format="square"
+              />
+              <Avatar size="medium" format="square" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Small Avatar */}
+      <div className="flex translate-x-2 flex-row items-center gap-10">
+        <Label variant="group">Small</Label>
+
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-row items-center gap-6 align-middle">
+            <Label>Circle</Label>
+            <div className="flex translate-x-4 gap-14">
+              <Avatar
+                src="https://github.com/MatheusFilg.png"
+                size="small"
+                format="circle"
+              />
+              <Avatar size="small" format="circle" />
+            </div>
+          </div>
+
+          <div className="flex flex-row items-center align-middle">
+            <Label>Square</Label>
+            <div className="flex translate-x-10 flex-row gap-14">
+              <Avatar
+                src="https://github.com/MatheusFilg.png"
+                size="small"
+                format="square"
+              />
+              <Avatar size="small" format="square" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center align-middle">
+        <div className="w-52" />
+        <div className="flex -translate-y-10 translate-x-16 flex-row gap-10">
+          <div className="flex items-center gap-1">
+            <Label variant="vertical">Image</Label>
+            <Label variant="vertical">Initial</Label>
           </div>
         </div>
       </div>
